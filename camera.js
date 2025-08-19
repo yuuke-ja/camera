12f770928a59a552375bcb6f030def0a952e7229
優介 <youjiezhongwei2@gmail.com>
2025-08-19 14:33:02 +0900

    Add files via upload

--- camera.js
+++ camera.js
@@ -124,7 +124,7 @@ snapBtn.addEventListener('click', () => {
     const h = w * (santaHatImg.height / santaHatImg.width);
     const x = (overlay.width - w) / 2;
     const y = overlay.height * 0.0; // 修正済み
-    overlayCtx.drawImage(santaHatImg, x, y, w, h);
+    ctx.drawImage(santaHatImg, x, y, w, h);
   }
 
   canvas.toBlob(blob => {
@@ -201,7 +201,7 @@ recordBtn.addEventListener('click', () => {
         const h = w * (santaHatImg.height / santaHatImg.width);
         const x = (overlay.width - w) / 2;
         const y = overlay.height * 0.0; // 修正済み
-        overlayCtx.drawImage(santaHatImg, x, y, w, h);
+        ctx.drawImage(santaHatImg, x, y, w, h);
       }
 
       if (recording) requestAnimationFrame(drawFrame);
