f5fefe53ca116fdc23a6210a686233b6a2c07fc8
優介 <youjiezhongwei2@gmail.com>
2025-08-24 17:25:57 +0900

    Add files via upload

--- camera.js
+++ camera.js
@@ -183,12 +183,12 @@ snapBtn.addEventListener('click', () => {
     // 縦なら
     if (portrait) {
       if (flameEl === flame) {
-        xRatio = 0.18;  // flameのX位置
-        yRatio = 0.28; // flameのY位置
+        xRatio = 0.17;  // flameのX位置
+        yRatio = 0.26; // flameのY位置
         sizeRatio = 0.5; // flameのサイズ
       } else if (flameEl === flame2) {
-        xRatio = 0.48;
-        yRatio = 0.28;
+        xRatio = 0.45;
+        yRatio = 0.26;
         sizeRatio = 0.5;
       }
     }
